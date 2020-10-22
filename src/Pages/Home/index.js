d98a60bcdfb4e0166e248f47a7368c5da10b621d
Jose Ricardo Carvalho Motta Miguel <ricardocarvalho@MacBook-Air-de-Jose.local>
2020-10-22 19:43:01 -0300

    feat: 🎸 Install dependencies and write readme of project
    Install styled components, router, eslint and prettier to inicialize
    project
    
    BREAKING CHANGE: 🧨 n

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Wrapper, Title } from './styles';
+
+function Home() {
+  return (
+    <Wrapper>
+      <Title>Home</Title>
+    </Wrapper>
+  )
+}
+
+export default Home
