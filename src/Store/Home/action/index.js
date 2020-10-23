c9dfec59b55b7f33cee142c95fa3a8905c486670
Ricardo Carvalho <ricardocarvalho@MacBook-Air-de-Jose.local>
2020-10-23 12:37:08 -0300

    feat: 🎸 Redux
    Include Redux on application and create reducer for Home

---
+++ src/Store/Home/action/index.js
@@ -0,0 +1,46 @@
+export const FETCH_EXPRESSION_BEGIN = 'FETCH_EXPRESSION_BEGIN'
+export const FETCH_EXPRESSION_SUCCESS = 'FETCH_EXPRESSION_SUCCESS'
+export const FETCH_EXPRESSION_FAILURE = 'FETCH_EXPRESSION_FAILURE'
+
+const fetchInvestmentBegin = () => ({
+  type: FETCH_EXPRESSION_BEGIN,
+})
+
+const fetchInvestmentSuccess = (investment) => ({
+  type: FETCH_EXPRESSION_SUCCESS,
+  payload: { investment },
+})
+
+const fetchInvestmentFailure = (error) => ({
+  type: FETCH_EXPRESSION_FAILURE,
+  payload: { error },
+})
+
+export const fetchInvestment = (formData) => {
+  const interestRate = 0.00517
+  const { monthlyPayment, time } = formData
+  const url = 'http://api.mathjs.org/v4/'
+  const settings = {
+    method: 'POST',
+    body: JSON.stringify({
+      expr: `${monthlyPayment} * (((1 + ${interestRate}) ^ ${time} - 1) / ${interestRate})`,
+    }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  }
+
+  return (dispatch) => {
+    dispatch(fetchInvestmentBegin())
+    fetch(url, settings)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          throw res.error
+        }
+        dispatch(fetchInvestmentSuccess(res.result))
+      })
+      .then()
+      .catch((error) => {
+        dispatch(fetchInvestmentFailure(error))
+      })
+  }
+}
